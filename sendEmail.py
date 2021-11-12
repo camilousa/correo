4cf27111794ef2bab2c3c5ed96b40b13e1f62c66
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2021-11-12 16:34:41 -0500

    lectura de datos del curso y renderizado del html

--- sendEmail.py
+++ sendEmail.py
@@ -1,5 +1,10 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
 import pandas as pd
 
+env = Environment(
+        loader=PackageLoader("sendEmail"),
+        autoescape=select_autoescape()
+    )
 
 def send_email():
     pass
@@ -8,17 +13,23 @@ def read_data(path, sheet, header=0):
     df = pd.read_excel(path, sheet, header=header)
     return df
 
-def render_template():
-    pass
+def render_template(template, data, meta):
+    template = env.get_template(template)
+    return template.render(data=data, meta=meta)
 
-def main(data_path, data_sheet, meta_sheet):
+def main(data_path, data_sheet, meta_sheet, template):
     data = read_data(data_path, data_sheet)
-    meta_data = read_data(data_path, meta_sheet)
-    print(meta_data)
-
+    meta_data = read_data(data_path, meta_sheet).to_dict('records')[0]
+    for index, row in data.iterrows():
+        d = row.to_dict()
+        d.pop('id')
+        html = render_template(template, d, meta_data)
+        print(html)
+        break
 
 if __name__ == '__main__':
     data_path = './sample_data/Aprendizaje-máquina-corte2.xlsx'
     data_sheet = 'mlcorte2'
     meta_sheet = 'email'
-    main(data_path, data_sheet, meta_sheet)
+    template = 'template_camilo.html'
+    main(data_path, data_sheet, meta_sheet, template)
